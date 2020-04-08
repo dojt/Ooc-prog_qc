2f7150d7177b95a324ffe86a19f269c7a9dc818e
Masoud Ataei <msd.ataei@gmail.com>
2020-04-07 21:43:51 -0400

    ass01

---
+++ assignment-01/Zomok_Dragon/a01.py
@@ -0,0 +1,32 @@
+##########################
+#######Zomok Dragon#######
+##########################
+
+from qiskit import QuantumCircuit
+from qiskit import BasicAer, execute
+
+def make_ghz3(shots, device):
+    # Make a quantum circuit for the GHZ state.
+    qubits_number = 3
+    qc = QuantumCircuit(qubits_number, qubits_number)
+
+    # Create a GHZ state
+    qc.h(0)
+    for i in range(qubits_number-1):
+        qc.cx(i, i+1)
+
+    # Insert a barrier before measurement
+    qc.barrier()
+    
+    # Measure all of the qubits in the standard basis
+    for i in range(qubits_number):
+        qc.measure(i, i)
+
+    sim_backend = BasicAer.get_backend(device)
+    job = execute(qc, sim_backend, shots=shots)
+    return job.result()
+
+# in the assignment it says 
+# "The function should return an object of the class Result" 
+# I am returning the result but I think 
+# the result is useless without having QuantumCircuit qc!
---
+++ assignment-01/Zomok_Dragon/a01_ibmq.py
@@ -0,0 +1,38 @@
+##########################
+#######Zomok Dragon#######
+##########################
+
+from qiskit import QuantumCircuit
+from qiskit import BasicAer, execute
+from qiskit import IBMQ
+
+# API token is needed here
+IBMQ.save_account('<your_API_token>')
+IBMQ.load_account()
+
+def make_ghz3_ibmq(shots, device):
+    # Make a quantum circuit for the GHZ state.
+    qubits_number = 3
+    qc = QuantumCircuit(qubits_number, qubits_number)
+
+    # Create a GHZ state
+    qc.h(0)
+    for i in range(qubits_number-1):
+        qc.cx(i, i+1)
+
+    # Insert a barrier before measurement
+    qc.barrier()
+    
+    # Measure all of the qubits in the standard basis
+    for i in range(qubits_number):
+        qc.measure(i, i)
+
+    provider = IBMQ.get_provider('ibm-q')
+    qcom = provider.get_backend(device)
+    
+    job = execute(qc, backend=qcom, shots=shots)
+    return job.result()
+
+# Same as the simulator version:
+# I am returning the result as assignment asks but I think
+# the result is useless without having QuantumCircuit qc!
